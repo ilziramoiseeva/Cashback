2528d8130dfea9ee481ae9f40953d9449007ebda
ilziramoiseeva <ilzirkam@gmail.com>
2020-10-05 18:54:39 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int regularPercent = 1;
+        int increasedPercent = 5;
+        int specialPercent = 30;
+        int regularCategoryCost = 2_300_00;
+        int increasedCategoryCost = 3_000_00;
+        int specialCategoryCost = 500_00;
+        int maxCashback = 3000_00;
+        int regularCashback = regularCategoryCost * regularPercent / 100;
+        int increasedCashback = increasedCategoryCost * increasedPercent / 100;
+        int specialCashback = specialCategoryCost * specialPercent / 100;
+        int allCashback = regularCashback + increasedCashback + specialCashback;
+        if (allCashback > maxCashback) {
+            System.out.println(maxCashback);
+        }
+        System.out.println(allCashback);
+    }
+}
