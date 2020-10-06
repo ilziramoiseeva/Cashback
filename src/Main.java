b0d0729957e96875066e9220536668f1825a0044
ilziramoiseeva <ilzirkam@gmail.com>
2020-10-06 20:32:16 +0300

    fixed #1

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
         int specialPercent = 30;
         int regularCategoryCost = 2_300_00;
         int increasedCategoryCost = 3_000_00;
-        int specialCategoryCost = 500_00;
+        int specialCategoryCost = 10500_00;
         int maxCashback = 3000_00;
         int regularCashback = regularCategoryCost * regularPercent / 100;
         int increasedCashback = increasedCategoryCost * increasedPercent / 100;
         int specialCashback = specialCategoryCost * specialPercent / 100;
         int allCashback = regularCashback + increasedCashback + specialCashback;
         if (allCashback > maxCashback) {
-            System.out.println(maxCashback);
+            allCashback=maxCashback;
         }
         System.out.println(allCashback);
     }
